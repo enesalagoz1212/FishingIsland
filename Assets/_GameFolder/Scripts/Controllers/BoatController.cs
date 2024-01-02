2a3ee755f8f81134cd6abb28099535f14761620d
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-01-02 16:30:47 +0300

    create

--- Assets/_GameFolder/Scripts/Controllers/BoatController.cs
+++ Assets/_GameFolder/Scripts/Controllers/BoatController.cs
@@ -22,36 +22,45 @@ namespace FishingIsland.Controllers
 		private int _fishCapacity = 0;
 		private int _maxFishCapacity = 10;
 
+		public Image countDown;
+		public TextMeshProUGUI timerText;
+		private bool _isTimerRunning = false;
+		private float _currentTime = 8;
 		public TextMeshProUGUI fishCapacityText;
+		public GameObject dockTimerPanel;
+		public TextMeshProUGUI boxFishText;
+		private int _totalFishCount = 0;
 		public void Initialize()
 		{
 			_initialPosition = transform.position;
+
+
 		}
 
 		private void Awake()
 		{
-			BoatState = BoatState.InThePort;
+			ChangeState(BoatState.InThePort);
+			TimerText();
 		}
 
 		public void ChangeState(BoatState boatState)
 		{
 			BoatState = boatState;
 			Debug.Log($"BoatState: {boatState}");
-		}
-
-
-
-		private void Update()
-		{
 			if (GameManager.Instance.GameState == GameState.Playing)
 			{
 				switch (BoatState)
 				{
 					case BoatState.InThePort:
 						transform.position = _initialPosition;
+					
 						break;
 					case BoatState.GoingFishing:
-						MoveToPosition(new Vector3(-40f, 0f, 0f), 3f);
+						_currentTime = 9f;
+						dockTimerPanel.gameObject.SetActive(true);
+						TimerText();
+						_isTimerRunning = true;
+						MoveToPosition(new Vector3(-40f, 0f, 0f), 2f);
 						DOVirtual.DelayedCall(3f, () =>
 						{
 							ChangeState(BoatState.Fishing);
@@ -60,20 +69,31 @@ namespace FishingIsland.Controllers
 					case BoatState.Fishing:
 						if (_fishCapacity < _maxFishCapacity)
 						{
-							_fishCapacity++;
-							UpdateFishCapacityText();
-						}
 
-						else
-						{
-							ChangeState(BoatState.ReturningToPort);
+							UpdateFishCapacityText();
 						}
 						break;
 					case BoatState.ReturningToPort:
-						MoveToPosition(_initialPosition, 3f);
+						MoveToPosition(_initialPosition, 1f);
+						_isTimerRunning = false;
+						
+						DOVirtual.DelayedCall(1f, () =>
+						{
+							dockTimerPanel.gameObject.SetActive(false);
+							UpdateTransferTheFishText();
+							UpdateGetTheFish();
+						});
+
 						break;
 				}
+
 			}
+		}
+
+		private void Update()
+		{
+			TimerControl();
+
 		}
 
 		private void MoveToPosition(Vector3 targetPosition, float duration)
@@ -89,7 +109,38 @@ namespace FishingIsland.Controllers
 
 		private void UpdateFishCapacityText()
 		{
+			_fishCapacity++;
 			fishCapacityText.text = $" {_maxFishCapacity}";
+		}
+
+		private void UpdateTransferTheFishText()
+		{
+			_fishCapacity= 0;
+			fishCapacityText.text = $" {_fishCapacity}";
+		}
+
+		private void UpdateGetTheFish()
+		{
+			_totalFishCount += _maxFishCapacity;
+			boxFishText.text = $" {_totalFishCount}";
+		}
+
+		private void TimerControl()
+		{
+			if (_isTimerRunning && _currentTime > 0)
+			{
+				_currentTime -= Time.deltaTime;
+				timerText.text = ((int)_currentTime).ToString();
+			}
+			if (_isTimerRunning && _currentTime <= 1)
+			{
+				ChangeState(BoatState.ReturningToPort);
+			}
+		}
+
+		private void TimerText()
+		{
+			timerText.text = _currentTime.ToString();
 		}
 	}
 
